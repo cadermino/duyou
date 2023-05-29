a222aecb9a5e22ee8e5fc6843baa09abae435482
carlos calderon <cadermino@gmail.com>
2023-05-29 13:08:23 -0500

    add hero section

--- src/components/home/hero.js
+++ src/components/home/hero.js
@@ -9,75 +9,114 @@ export function Hero() {
       px-5
       pt-8
       pb-16
-      flex 
-      flex-col
-      md:flex-row
-      md:justify-between
-      justify-center
+      md:pt-20
       bg-blue-dark-duyou
       ">
-      <h1 className="
-        basis-1/2
-        font-avenir-light
-        text-[40px]
-        leading-10
-        pb-4
-        ">Acelera tu negocio de manera eficiente <span className="
-                                                  font-avenir-heavy">con logística de China a México</span></h1>
       <div className="
-        basis-1/2
         flex 
+        flex-col
+        md:flex-row
+        md:justify-between
         justify-center
         ">
+        <h1 className="
+          basis-1/2
+          font-avenir-light
+          text-[40px]
+          2xl:text-[80px]
+          2xl:leading-[5rem]
+          leading-10
+          pb-4
+          ">Acelera tu negocio de manera eficiente 
+          <span className="
+            font-avenir-heavy">con logística de China a México</span>
+          <div className="
+            hidden
+            md:block
+            text-sm
+            2xl:text-xl
+            mt-5
+            ">Tenemos soluciones personalizadas para tu negocio.</div>
+        </h1>
         <div className="
-          max-w-[612px]
-          max-h-[313px]
-          w-[358px]
-          h-[200px]
-          lg:w-[612px]
-          lg:h-[313px]
-          block
-          relative
+          basis-1/2
+          flex 
+          justify-center
           ">
-          <Image
-            src="https://placehold.co/358x200.png"
-            // width={358}
-            // height={200}
-            fill
-            alt="Duyou Logo"
+          <div className="
+            max-w-[612px]
+            max-h-[313px]
+            w-[358px]
+            h-[200px]
+            lg:w-[612px]
+            lg:h-[313px]
+            block
+            relative
+            ">
+            <Image
+              src="https://placehold.co/358x200.png"
+              fill
+              alt="Duyou Logo"
 
-          />
+            />
+          </div>
+        </div>
+        <h2 className="
+          md:hidden
+          font-avenir-light
+          text-lg
+          text-center
+          pt-4
+          ">Tenemos soluciones personalizadas para tu negocio.</h2>
+
+        <div className="
+          md:hidden
+          mt-4
+          text-lg
+          font-avenir-light
+          text-center
+          underline
+          decoration-1">
+          <Link href="test">Ver video</Link>
+        </div>
+        <div className="
+          md:hidden
+          mt-8
+          text-center">
+          <Link href="test" className="
+            rounded-lg
+            py-3
+            px-20
+            bg-red-duyou-primary
+            text-lg
+            ">Cotizar envío</Link>
         </div>
       </div>
-      <h2 className="
-        md:hidden
-        font-avenir-light
-        text-lg
+      <div className="
+        2xl:text-xl
+        mt-16
         text-center
-        pt-4
-        ">Tenemos soluciones personalizadas para tu negocio.</h2>
+        flex flex-col justify-center
+        ">
+        <Link href="test">
+          <div>
+            <div className="
+              w-[110.2px]
+              h-[63.35px]
+              hidden
+              md:inline-block
+              relative
+              ">
+              <Image
+                src="https://placehold.co/111x64.png"
+                fill
+                alt="youtube"
 
-      <div className="
-        md:hidden
-        mt-4
-        text-lg
-        font-avenir-light
-        text-center
-        underline
-        decoration-1">
-        <Link href="test">Ver video</Link>
-      </div>
-      <div className="
-        md:hidden
-        mt-8
-        text-center">
-        <Link href="test" className="
-          rounded-lg
-          py-3
-          px-20
-          bg-red-duyou-primary
-          text-lg
-          ">Cotizar envío</Link>
+              />
+            </div>
+          </div>  
+          <div>Ver video</div>
+        </Link>
       </div>
     </article>
   );
